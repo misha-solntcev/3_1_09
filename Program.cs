0e54b8baee83843752483d27f64b4ad7865a21d8
misha-solntcev <misha.solntcev@gmail.com>
2022-11-28 17:22:12 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 9. Дано целое число N (> 1). Найти наименьшее целое число K, 
+    при котором выполняется неравенство 3K > N. */
+
+namespace _3_1_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 12;
+            int K = 0;
+            while (3 * K <= N)
+            {                
+                K++;
+                Console.WriteLine(K);
+            }            
+            Console.ReadKey();
+        }
+    }
+}
